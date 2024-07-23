4055d266a0ae2689484626cf99a3123f652c544c
Michael <mishanhzer@gmail.com>
2024-07-23 09:27:35 +0500

    add transform api data

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,8 +1,8 @@
+// Если наше приложение взаимодействует с сервером, то отделяем сетевые компоненты от основного кода (трансформацию данных тоже добавляем в серивисы)
 
 class MarvelService { 
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=ef4b5738e027c92c944977fe97f44c1e'
-
 
     getResource = async (url) => { 
         let res = await fetch(url); 
@@ -14,12 +14,24 @@ class MarvelService {
         return await res.json(); 
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=205&${this._apiKey}`); 
+    getAllCharacters = async () => { 
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=205&${this._apiKey}`); 
+        return res.data.results.map(this._transformCharacter); 
     }
 
-    getCharacter = (id) => { 
-        return this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`); 
+    getCharacter = async (id) => { 
+        const res = await this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`); 
+        return this._transformCharacter(res.data.results[0]); 
+    }
+
+    _transformCharacter = (char) => { 
+        return { 
+            name: char.name, 
+            description: char.description, 
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`, 
+            homepage: char.urls[0].url, 
+            wiki: char.urls[1].url 
+        }
     }
 }
 
